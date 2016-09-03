f0a81595feab0350c3f44f607a70e428ccb48c3d
Damien <DamienAlbino@yahoo.com>
2016-09-03 07:03:08 -0400

    modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   
   get 'about' => 'pages#about'
   
-  get 'destroy_user_session' => 'devise/sessions#destroy'  
+  get 'users/sign_out' => 'devise/sessions#destroy'  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
